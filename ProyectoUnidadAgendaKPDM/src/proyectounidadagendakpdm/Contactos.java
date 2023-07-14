742fa10474726f9bf709ac91402972a8633d5099
kimpamela-1239506 <kimpameladiaz0613@gmail.com>
2023-07-14 11:45:26 -0600

    Hola!

--- ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/Contactos.java
+++ ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/Contactos.java
@@ -3,4 +3,12 @@ package proyectounidadagendakpdm;
 
 public class Contactos {
     
+    private String nombre, domicilio,numerotel;
+    
+    public Contactos(String nombre, String domicilio, String numerotel){
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.numerotel = numerotel;
+        
+    }
 }
