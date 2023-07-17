41d4f2f98f88530b1f0bd78ed0e8ab4332a55091
kimpamela-1239506 <kimpameladiaz0613@gmail.com>
2023-07-17 11:08:06 -0600

    Actualizacion de Funciones

--- ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/Contactos.java
+++ ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/Contactos.java
@@ -1,14 +1,30 @@
-
 package proyectounidadagendakpdm;
 
 public class Contactos {
-    
-    private String nombre, domicilio,numerotel;
-    
-    public Contactos(String nombre, String domicilio, String numerotel){
+
+    //atributos del contacto
+    private String nombre, domicilio, numerotel;
+
+    public Contactos(String nombre, String domicilio, String numerotel) {
+
         this.nombre = nombre;
         this.domicilio = domicilio;
         this.numerotel = numerotel;
-        
+
+    }
+
+    public String verNombre() {
+        return this.nombre;
+
+    }
+
+    public String verDomicilio() {
+        return this.domicilio;
+
+    }
+
+    public String verNumero() {
+        return this.numerotel;
+
     }
 }
--- ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/ProyectoUnidadAgendaKPDM.java
+++ ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/ProyectoUnidadAgendaKPDM.java
@@ -8,12 +8,19 @@ número de teléfono, nombre y domicilio. Para buscar un contacto se debe realiz
 número de teléfono.*/
 public class ProyectoUnidadAgendaKPDM {
 
+    static Contactos misContactos[] = new Contactos[15];
+
     static int contador = 0;
 
     public static void main(String[] args) {
+
         int op = 0;
+
         Scanner entrada = new Scanner(System.in);
+        System.out.println("----------------- AGENDA TELEFONICA -------------------");
+
         while (op != 4) {
+            System.out.println("");
             System.out.println("--------- MENU ---------");
             System.out.println("1. Guardar Contacto");
             System.out.println("2. Ver Todos Los Contactos");
@@ -21,36 +28,61 @@ public class ProyectoUnidadAgendaKPDM {
             System.out.println("4. Salir");
             System.out.println("");
             System.out.println("Ingrese el numero de opcion:");
+
             op = entrada.nextInt();
+
             System.out.println("------------------------------------");
-            if (op == 1) {
-                System.out.println("----- Nuevo Contacto -----");
-            } else if (op == 2) {
-                System.out.println("----- Los Datos Del Contacto Son: -----");
-            } else if (op == 3) {
-                System.out.println("Progama Finalizado");
-            } else {
-                System.out.println("Opcion incorrecta");
+            switch (op) {
+
+                case 1 ->
+                    guardarContacto();
+
+                case 2 ->
+                    verTodos();
+
+//Se busca el contacto por el numero de telefono
+                case 3 -> {
+
+                    System.out.println("----- BUSCAR CONTACTO -----");
+
+                    System.out.println("Ingrese el nombre del contacto porfavor: ");
+
+                    String nombre = entrada.nextLine();
+
+                    System.out.println(verContacto(nombre));
+
+//Salir del programa
+                }
+
+                case 4 ->
+                    System.out.println("Progama Finalizado");
+
+                default ->
+                    System.out.println("Opcion incorrecta");
 
             }
 
         }
+
     }
 
+//crear nuevo contacto
     static public void guardarContacto() {
 
-        if (contador >= 2) {
+//al llegar a 15, aparece
+        if (contador >= 15) {
 
             System.out.println("---------------------------------------");
-
             System.out.println("-------- La Agenda Esta Llena --------");
-
             System.out.println("---------------------------------------");
 
+//si no, se ingresan los datos del nuevo contacto
         } else {
+
             Scanner teclado = new Scanner(System.in);
 
-            System.out.println("-------------- Nuevo Contacto --------------");
+            System.out.println("----------- Nuevo Contacto -----------");
+
             System.out.println("Ingrese el nombre del Contacto");
             String nombre = teclado.nextLine();
 
@@ -60,7 +92,56 @@ public class ProyectoUnidadAgendaKPDM {
             System.out.println("Ingrese el numero del Contacto");
             String numerotel = teclado.nextLine();
 
-            System.out.println("Dato almacenado");
+//se almacena el dato con los atributos
+            System.out.println("--- Dato almacenado! ---");
+
+            misContactos[contador] = new Contactos(nombre, domicilio, numerotel);
+
+            contador++;
+
+        }
+
+    }
+
+//ver los datos del contacto
+    static public void verTodos() {
+
+        for (int i = 0; i < contador; i++) {
+
+            System.out.println("Nombre: " + misContactos[i].verNombre());
+
+            System.out.println("Domicilio: " + misContactos[i].verDomicilio());
+
+            System.out.println("Numero de telefono: " + misContactos[i].verNumero());
+
+            System.out.println("------------------------------------------------------------");
+
+        }
+
+    }
+
+//buscar el contacto de manera individual
+    static public String verContacto(String nombre) {
+
+        int posicion = -1;
+
+        for (int i = 0; i < 15; i++) {
+            if (misContactos[i].verNombre().equals(nombre)) {
+                posicion = i;
+
+                break;
+
+            }
+
+        }
+
+        if (posicion != -1) {
+            return "Numero: " + misContactos[posicion].verNumero() + " Domicilio: " + misContactos[posicion].verDomicilio();
+
+        } else {
+
+            return "OPS!"
+                    + "-- Lo sentimos! No hemos podido encontral al contacto --";
 
         }
 
