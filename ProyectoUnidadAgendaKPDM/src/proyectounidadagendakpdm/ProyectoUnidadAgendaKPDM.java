1cca0e3c7520c5d60cc59f97b7430aed9a68ceff
kimpamela-1239506 <139266345+kimpamela-1239506@users.noreply.github.com>
2023-07-20 22:22:18 -0600

    Agregado opcion modificar
    Tambien se eliminaro lineas, se agrego variables y comentarios.

--- ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/ProyectoUnidadAgendaKPDM.java
+++ ProyectoUnidadAgendaKPDM/src/proyectounidadagendakpdm/ProyectoUnidadAgendaKPDM.java
@@ -9,141 +9,135 @@ número de teléfono.*/
 public class ProyectoUnidadAgendaKPDM {
 
     static Contactos misContactos[] = new Contactos[15];
-
     static int contador = 0;
+    //int, Strings para ejecutar la funcion modificar
+    static int num;
+    static String dom, nom;
 
     public static void main(String[] args) {
-
         int op = 0;
-
         Scanner entrada = new Scanner(System.in);
         System.out.println("----------------- AGENDA TELEFONICA -------------------");
-
-        while (op != 4) {
+        while (op != 5) {
             System.out.println("");
             System.out.println("--------- MENU ---------");
             System.out.println("1. Guardar Contacto");
             System.out.println("2. Ver Todos Los Contactos");
             System.out.println("3. Buscar un Contacto");
-            System.out.println("4. Salir");
-            System.out.println("");
-            System.out.println("Ingrese el numero de opcion:");
-
+            System.out.println("4. Modificar Contacto");
+            System.out.println("5. Salir");
+            System.out.println("\nIngrese el numero de opcion:");
             op = entrada.nextInt();
-
             System.out.println("------------------------------------");
-            switch (op) {
-
-                case 1 ->
-                    guardarContacto();
-
-                case 2 ->
-                    verTodos();
-
+            if (op == 1) {
+                guardarContacto();
+            } else if (op == 2) {
+                verTodos();
+            } else if (op == 3) {
 //Se busca el contacto por el numero de telefono
-                case 3 -> {
-
-                    System.out.println("----- BUSCAR CONTACTO -----");
-
-                    System.out.println("Ingrese el nombre del contacto porfavor: ");
-
-                    String nombre = entrada.nextLine();
-
-                    System.out.println(verContacto(nombre));
-
+                System.out.println("----- BUSCAR CONTACTO -----");
+                System.out.println("Ingrese el nombre del contacto porfavor: ");
+                Scanner entrada2 = new Scanner(System.in);
+                String nombre = entrada2.nextLine();
+                System.out.println(verContacto(nombre));
 //Salir del programa
-                }
-
-                case 4 ->
-                    System.out.println("Progama Finalizado");
-
-                default ->
-                    System.out.println("Opcion incorrecta");
-
+            } else if (op == 4) {
+                System.out.println("--MODIFICAR CONTACTO--");
+                System.out.println("Ingrese el numero del contacto que desea modificar porfavor: ");
+                Scanner entrada3 = new Scanner(System.in);
+                String numero = entrada3.nextLine();
+                System.out.println(modificarContacto(numero));
+                System.out.println("nombre: " + misContactos[num].verNombre());
+                System.out.println("numero: " + misContactos[num].verNumero());
+                System.out.println("domicilio: " + misContactos[num].verDomicilio());
+                System.out.println("------------------------------------");
+            } else if (op == 5) {
+                System.out.println("Progama Finalizado");
+            } else {
+                System.out.println("Opcion incorrecta");
             }
-
         }
-
     }
-
 //crear nuevo contacto
-    static public void guardarContacto() {
 
+    static public void guardarContacto() {
 //al llegar a 15, aparece
         if (contador >= 15) {
-
             System.out.println("---------------------------------------");
             System.out.println("-------- La Agenda Esta Llena --------");
             System.out.println("---------------------------------------");
 
 //si no, se ingresan los datos del nuevo contacto
         } else {
-
             Scanner teclado = new Scanner(System.in);
-
             System.out.println("----------- Nuevo Contacto -----------");
-
             System.out.println("Ingrese el nombre del Contacto");
             String nombre = teclado.nextLine();
-
             System.out.println("Ingrese el domicilio del Contacto");
             String domicilio = teclado.nextLine();
-
             System.out.println("Ingrese el numero del Contacto");
             String numerotel = teclado.nextLine();
-
 //se almacena el dato con los atributos
             System.out.println("--- Dato almacenado! ---");
-
             misContactos[contador] = new Contactos(nombre, domicilio, numerotel);
-
             contador++;
-
         }
-
     }
 
 //ver los datos del contacto
     static public void verTodos() {
-
         for (int i = 0; i < contador; i++) {
-
             System.out.println("Nombre: " + misContactos[i].verNombre());
-
             System.out.println("Domicilio: " + misContactos[i].verDomicilio());
-
             System.out.println("Numero de telefono: " + misContactos[i].verNumero());
-
             System.out.println("------------------------------------------------------------");
-
         }
-
     }
 
 //buscar el contacto de manera individual
     static public String verContacto(String nombre) {
-
         int posicion = -1;
-
         for (int i = 0; i < 15; i++) {
-            if (misContactos[i].verNombre().equals(nombre)) {
-                posicion = i;
-
-                break;
-
+            if (misContactos[i] != null) {
+                if (misContactos[i].verNombre().equals(nombre)) {
+                    posicion = i;
+                    break;
+                }
             }
-
         }
-
         if (posicion != -1) {
             return "Numero: " + misContactos[posicion].verNumero() + " Domicilio: " + misContactos[posicion].verDomicilio();
-
         } else {
-
             return "OPS!"
-                    + "-- Lo sentimos! No hemos podido encontral al contacto --";
-
+                    + "-- Lo sentimos! No hemos podido encontrar el contacto --";
         }
+    }
+//modificar el contacto
 
+    static public String modificarContacto(String numero) {
+        Scanner tec = new Scanner(System.in);
+        int posicion = -1;
+        for (int i = 0; i < 15; i++) {
+            if (misContactos[i] != null) {
+                if (misContactos[i].verNumero().equals(numero)) {
+                    posicion = i;
+                    num = i;
+                    break;
+                }
+            }
+        }
+        if (posicion != -1) {
+            System.out.println("------------------------------------");
+            System.out.println("Ingrese el nombre del Contacto");
+            nom = tec.nextLine();
+            System.out.println("Ingrese el domicilio del Contacto");
+            dom = tec.nextLine();
+            misContactos[num].cambiarNom(nom);
+            misContactos[num].cambiarDom(dom);
+            return "--- Dato almacenado con exito! ---";
+        } else {
+            return "OPS!"
+                    + "-- Lo sentimos! No hemos podido encontrar el contacto --";
+        }
     }
 }
